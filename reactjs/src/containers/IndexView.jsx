7159d2864a720e52860857e7e5da4bd5d561d12e
Martin Brochhaus <mbrochh@gmail.com>
2016-03-28 23:20:00 +0800

    Added Skills whishlist to IndexView

--- reactjs/src/containers/IndexView.jsx
+++ reactjs/src/containers/IndexView.jsx
@@ -1,8 +1,11 @@
 import React from "react"
 import { connect } from "react-redux"
 
+import Button from "../components/Button"
 import Jumbotron from "../components/Jumbotron"
 import Skill from "../components/Skill"
+import Panel from "../components/Panel"
+import InputText from "../components/InputText"
 
 
 export default class IndexView extends React.Component {
@@ -21,6 +24,25 @@ export default class IndexView extends React.Component {
           </div>
           <div className="col-sm-4">
             <Skill title="Web-Frontend Development" />
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <div className="col-sm-6">
+            <Panel heading="Skills on the whishlist">
+              <div className="row">
+                <div className="col-sm-4">
+                  <p>Skill Name 1 (15) <span className="glyphicon glyphicon-thumbs-up"></span></p>
+                  <p>Skill Name 2 (2) <span className="glyphicon glyphicon-thumbs-up"></span></p>
+                </div>
+              </div>
+            </Panel>
+          </div>
+          <div className="col-sm-6">
+            <Panel heading="Something else?">
+              <InputText placeholder="Enter skill here" />
+              <Button>Submit</Button>
+            </Panel>
           </div>
         </div>
       </div>
