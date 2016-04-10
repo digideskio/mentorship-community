1359656b1947cb11b8238d8d8deaf120c16230dc
Martin Brochhaus <mbrochh@gmail.com>
2016-04-10 08:49:12 +0800

    New lead text for IndexView / removed skill selection from IndexView

--- reactjs/src/containers/IndexView.jsx
+++ reactjs/src/containers/IndexView.jsx
@@ -17,36 +17,10 @@ export default class IndexView extends React.Component {
         </Jumbotron>
         <div className="row">
           <div className="col-sm-offset-1 col-sm-10">
-            <div className="lead text-center">Programming is learned best while trying it at home, but self-teaching such a complex topic can be frustrating. Our goal is to structure your lessons, give you access to friendly mentors and introduce you to the broader communities.</div>
+            <div className="lead text-center">The best way to learn programming is at home and at your own pace. However, self-teaching such a complex topic can be daunting. Our goal is to structure your lessons, give you access to friendly mentors and introduce you to the broader communities.</div>
           </div>
         </div>
         <hr />
-        <div className="row">
-          <div className="col-sm-4">
-            <Skill title="Django Development" />
-          </div>
-          <div className="col-sm-4">
-            <Skill title="Git Basics" />
-          </div>
-          <div className="col-sm-4">
-            <Skill title="Python Basics" />
-          </div>
-          <div className="col-sm-4">
-            <Skill title="HTML & CSS Basics" />
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-sm-6">
-            <Panel heading="Skills on the whishlist">
-              <p>Skill Name 1 (15) <span className="glyphicon glyphicon-thumbs-up"></span></p>
-              <p>Skill Name 2 (2) <span className="glyphicon glyphicon-thumbs-up"></span></p>
-              <h3>Something else?</h3>
-              <InputText placeholder="Enter skill here" />
-              <Button>Submit</Button>
-            </Panel>
-          </div>
-        </div>
       </div>
     )
   }
