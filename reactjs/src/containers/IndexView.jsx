3db7dbf89bbb00bc1cc00a522318a19283b5f265
Martin Brochhaus <mbrochh@gmail.com>
2016-04-12 09:12:09 +0800

    Added four-step pitch to IndexView

--- reactjs/src/containers/IndexView.jsx
+++ reactjs/src/containers/IndexView.jsx
@@ -8,6 +8,16 @@ import Panel from "../components/Panel"
 import InputText from "../components/InputText"
 
 
+const styles = {
+  marginTop: {
+    marginTop: "2em",
+  },
+  icon: {
+    fontSize: "30px",
+  }
+}
+
+
 export default class IndexView extends React.Component {
   render() {
     return (
@@ -21,6 +31,60 @@ export default class IndexView extends React.Component {
           </div>
         </div>
         <hr />
+        <h1 className="text-center">How It Works</h1>
+        <div className="row" style={styles.marginTop}>
+          <div className="col-sm-3">
+            <div className="text-center">
+              <div className="glyphicon glyphicon-user" style={styles.icon}></div>
+              <h2 className="text-center">Sign up</h2>
+              <p>Simply come up with a username and sign up for an account.</p>
+            </div>
+          </div>
+          <div className="col-sm-3">
+            <div className="text-center">
+              <div
+                className="glyphicon glyphicon-screenshot"
+                style={styles.icon}
+              />
+              <h2 className="text-center">Pick a Skill</h2>
+              <p>
+                Select one of the available skills that you want to learn.
+                Some skills are so complex, they are based on several other
+                sub-skills. We show you nicely how it is all related to each
+                other.
+              </p>
+            </div>
+          </div>
+          <div className="col-sm-3">
+            <div className="text-center">
+              <div
+                className="glyphicon glyphicon-book"
+                style={styles.icon}
+              />
+              <h2 className="text-center">Teach Yourself</h2>
+              <p>
+                We break down each skill into very small tasks and tell you
+                what you need to learn. You take your time and when you think
+                you are ready, you do a little test.
+              </p>
+            </div>
+          </div>
+          <div className="col-sm-3">
+            <div className="text-center">
+              <div
+                className="glyphicon glyphicon-map-marker"
+                style={styles.icon}
+              />
+              <h2 className="text-center">Meet Your Mentor</h2>
+              <p>
+                Once you have learned a few modules, you will meet your mentor
+                in a small group and discuss the topics you have learned. This
+                will make sure that you got it all right and that you meet
+                interesting people in the community.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
