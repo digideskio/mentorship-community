c66aafdbc541b6a5517bd21bb89a46f01fa115a4
Martin Brochhaus <mbrochh@gmail.com>
2016-05-02 17:38:29 +0800

    Added prod webpack config

--- django/fabfile.py
+++ django/fabfile.py
@@ -5,3 +5,5 @@ def webpack():
     local('rm mentorship_community/static/bundles/*')
     local('cd ../reactjs && node_modules/.bin/webpack'
           ' --config webpack.local.config.js')
+    local('cd ../reactjs && node_modules/.bin/webpack'
+          ' --config webpack.prod.config.js')
---
+++ reactjs/webpack.prod.config.js
@@ -0,0 +1,29 @@
+var path = require("path")
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+var ip = '188.166.208.127'
+var config = require('./webpack.base.config.js')
+
+config.plugins = config.plugins.concat([
+  new BundleTracker({filename: '../django/webpack-stats.json'}),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production'),
+      'BASE_API_URL': JSON.stringify('https://'+ip+'/api/v1/'),
+  }}),
+  // keeps hashes consistent between compilations
+  new webpack.optimize.OccurenceOrderPlugin(),
+  // minifies your code
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: {
+      warnings: false
+    }
+  })
+])
+
+config.module.loaders.push(
+  { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel'] }
+)
+
+module.exports = config
