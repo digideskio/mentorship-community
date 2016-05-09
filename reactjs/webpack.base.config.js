d8fc9989b6e1967e2a61276c88d3dd398f455f71
Martin Brochhaus <mbrochh@gmail.com>
2016-05-09 18:09:01 +0800

    Fixed entry point app name in webpack base config

--- reactjs/webpack.base.config.js
+++ reactjs/webpack.base.config.js
@@ -6,7 +6,7 @@ module.exports = {
 
   entry: {
     // Add as many entry points as you have container-react-components here
-    App: './src/App',
+    App1: './src/App',
     vendors: ['react'],
   },
 
