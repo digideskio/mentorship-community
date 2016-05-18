20ced8cddb06afc57ff99a57aa9f5e2f85c6a9d3
Martin Brochhaus <mbrochh@gmail.com>
2016-05-18 22:50:03 +0800

    Wrapping <ProfileView> in a className="container" div

--- reactjs/src/containers/ProfileView.jsx
+++ reactjs/src/containers/ProfileView.jsx
@@ -14,38 +14,39 @@ export default class ProfileView extends React.Component {
     return (
       <div>
         <Jumbotron><h1>Your Profile</h1></Jumbotron>
-
-        <div className="row">
-          <div className="col-sm-6">
-            <Panel heading="Edit Your Profile">
-              <form className="form">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <Panel heading="Edit Your Profile">
+                <form className="form">
+                  <p>
+                    <Avatar /> Upload Avatar
+                  </p>
+                  <InputText name="email" placeholder="Email" />
+                  <InputText name="username" placeholder="Username" />
+                  <InputText name="github_user" placeholder="Github User" />
+                  <InputText name="slack_user" placeholder="Slack User" />
+                  <InputText name="facebook_user" placeholder="Facebook User" />
+                  <InputText name="password" placeholder="Password" />
+                  <Button>Save</Button>
+                </form>
+              </Panel>
+            </div>
+            <div className="col-sm-6">
+              <Panel heading="Your Statistics">
+                <h2>Your Progress</h2>
+                <ProgressBox title="Git Basics: 8 of 12 modules" />
+                <ProgressBox title="Python Basics: 0 of 10 modules" />
+                <ProgressBox title="HTML & CSS Basics: 0 of 15 modules" />
+                <ProgressBox title="Django Development: 0 of 38 modules" />
+                <h2>Next Meetup</h2>
                 <p>
-                  <Avatar /> Upload Avatar
+                  There will be a meetup for [Skill Name & Track Name], soon!
+                  You only need to finish 3 modules in order to participate!
+                  You can do this :)
                 </p>
-                <InputText name="email" placeholder="Email" />
-                <InputText name="username" placeholder="Username" />
-                <InputText name="github_user" placeholder="Github User" />
-                <InputText name="slack_user" placeholder="Slack User" />
-                <InputText name="facebook_user" placeholder="Facebook User" />
-                <InputText name="password" placeholder="Password" />
-                <Button>Save</Button>
-              </form>
-            </Panel>
-          </div>
-          <div className="col-sm-6">
-            <Panel heading="Your Statistics">
-              <h2>Your Progress</h2>
-              <ProgressBox title="Git Basics: 8 of 12 modules" />
-              <ProgressBox title="Python Basics: 0 of 10 modules" />
-              <ProgressBox title="HTML & CSS Basics: 0 of 15 modules" />
-              <ProgressBox title="Django Development: 0 of 38 modules" />
-              <h2>Next Meetup</h2>
-              <p>
-                There will be a meetup for [Skill Name & Track Name], soon!
-                You only need to finish 3 modules in order to participate!
-                You can do this :)
-              </p>
-            </Panel>
+              </Panel>
+            </div>
           </div>
         </div>
       </div>
