f888bb39ab8231bb4720d1c265c801d09998db27
Martin Brochhaus <mbrochh@gmail.com>
2016-05-23 21:06:23 +0800

    Added skills app

---
+++ django/skills/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Skill(models.Model):
+    # name
+    prerequisites = models.ManyToManyRel('skills.Skill')
+    users = models.ManyToManyField('auth.User', related_name='modules')
+
+
+class Track(models.Model):
+    skill = models.ForeignKey('skills.Skill', related_name='tracks')
+
+
+class Module(models.Model):
+    track = models.ForeignKey('skills.Track', related_name='modules')
+    users = models.ManyToManyField('auth.User', related_name='modules')
+    # position (PositiveIntegerField)
+    # description
+    # instructions
