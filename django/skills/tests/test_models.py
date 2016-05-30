993477eab887743235c20901b4a6503d617da4ba
Vina Melody <vina_melody@yahoo.com>
2016-05-30 23:40:03 +0800

    Tidy up, save spaces

--- django/skills/tests/test_models.py
+++ django/skills/tests/test_models.py
@@ -6,21 +6,18 @@ pytestmark = pytest.mark.django_db
 
 
 class TestSkill:
-
     def test_init(self):
         obj = mixer.blend('skills.Skill')
         assert obj.pk == 1, 'Should create a skill object'
 
 
 class TestTrack:
-
     def test_init(self):
         obj = mixer.blend('skills.Track')
         assert obj.pk == 1, 'Should create a track object'
 
 
 class TestModule:
-
     def test_init(self):
         obj = mixer.blend('skills.Module')
         assert obj.pk == 1, 'Should create a module object'
