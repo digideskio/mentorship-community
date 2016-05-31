0fb1c32c2b74d37f6dfa3aac4856f9465c00def2
Vina Melody <vina_melody@yahoo.com>
2016-05-31 18:04:47 +0800

    Skills Django admin interface

--- django/skills/admin.py
+++ django/skills/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class SkillAdmin(admin.ModelAdmin):
+    model = models.Skill
+    fields = [
+        'name', 'prerequisites', 'users'
+    ]
+    raw_id_fields = ['users', ]
+
+
+class ModuleInline(admin.StackedInline):
+    model = models.Module
+    extra = 1
+    fields = [
+        ('name', 'position'), 'description', 'instructions', 'users'
+    ]
+    raw_id_fields = ['users', ]
+
+
+class TrackAdmin(admin.ModelAdmin):
+    model = models.Track
+    inlines = [ModuleInline, ]
+
+
+admin.site.register(models.Skill, SkillAdmin)
+admin.site.register(models.Track, TrackAdmin)
--- django/skills/models.py
+++ django/skills/models.py
@@ -4,20 +4,43 @@ from django.db import models
 
 
 class Skill(models.Model):
-    prerequisites = models.ManyToManyField('skills.Skill')
-    users = models.ManyToManyField('auth.User', related_name='skills')
-    name = models.TextField(
-        help_text='Skill Name'
+    prerequisites = models.ManyToManyField(
+        'skills.Skill',
+        blank=True
     )
+    users = models.ManyToManyField(
+        'auth.User',
+        related_name='skills',
+        blank=True)
+    name = models.CharField(
+        help_text='Skill Name',
+        max_length=512,
+        null=True,
+    )
+
+    def __unicode__(self):
+        return u'{}'.format(self.name)
 
 
 class Track(models.Model):
     skill = models.ForeignKey('skills.Skill', related_name='tracks')
+    name = models.CharField(
+        help_text='Track Name',
+        max_length=512,
+        null=True,
+    )
+
+    def __unicode__(self):
+        return u'{}'.format(self.name)
 
 
 class Module(models.Model):
     track = models.ForeignKey('skills.Track', related_name='modules')
-    users = models.ManyToManyField('auth.User', related_name='modules')
+    users = models.ManyToManyField(
+        'auth.User',
+        related_name='modules',
+        blank=True
+    )
     position = models.PositiveIntegerField()
     description = models.TextField(
         max_length=1024,
@@ -27,3 +50,11 @@ class Module(models.Model):
         max_length=1024,
         help_text='Instructions for student'
     )
+    name = models.CharField(
+        help_text='Module Name',
+        max_length=512,
+        null=True,
+    )
+
+    def __unicode__(self):
+        return u'{}'.format(self.name)
