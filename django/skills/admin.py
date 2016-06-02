24656ce8a3238e0e65598c59687a3822cab75b19
Martin Brochhaus <mbrochh@gmail.com>
2016-06-02 12:28:52 +0800

    Added Module admin

--- django/skills/admin.py
+++ django/skills/admin.py
@@ -18,6 +18,12 @@ class SkillAdmin(admin.ModelAdmin):
     users_count.short_description = 'Users Enrolled'
 
 
+class ModuleAdmin(admin.ModelAdmin):
+    model = models.Module
+    list_display = ('name', 'track', 'position')
+    raw_id_fields = ('users', 'track')
+
+
 class ModuleInline(admin.StackedInline):
     model = models.Module
     extra = 1
@@ -30,7 +36,9 @@ class ModuleInline(admin.StackedInline):
 class TrackAdmin(admin.ModelAdmin):
     model = models.Track
     inlines = [ModuleInline, ]
+    raw_id_fields = ('skill', )
 
 
 admin.site.register(models.Skill, SkillAdmin)
 admin.site.register(models.Track, TrackAdmin)
+admin.site.register(models.Module, ModuleAdmin)
